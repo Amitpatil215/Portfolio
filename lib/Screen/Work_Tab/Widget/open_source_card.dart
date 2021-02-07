baca741da50c817475609ece94e93a8ac29f5592
Amit Patil <amitgpatil215@gmail.com>
2021-02-07 11:05:51 +0530

    Open SOurce card updated

---
+++ lib/Screen/Work_Tab/Widget/open_source_card.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class OpenSourceCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      elevation: 10,
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 12,
+          left: 12,
+          right: 12,
+        ),
+        width: 370,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/flutter_logo.png'),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Flutter',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Text(
+                      'Cross platform UI toolkit',
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.watch_later_outlined,
+                          size: 18,
+                        ),
+                        SizedBox(width: 5),
+                        Text('Updated 2h ago'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Icon(Icons.plumbing_outlined),
+                SizedBox(width: 10),
+                Text(
+                  'Pull Requests',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Spacer(),
+                Text(
+                  '13',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 260,
+                ),
+                child: StaggeredGridView.countBuilder(
+                  physics: NeverScrollableScrollPhysics(),
+                  crossAxisCount: 4,
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int index) =>
+                      GestureDetector(
+                    onTap: () {
+                      print(index);
+                    },
+                    child: Container(
+                      color: Colors.amber,
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Text('$index'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  staggeredTileBuilder: (int index) =>
+                      StaggeredTile.count(2, index.isEven ? 2 : 1),
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                'Implemented Circle avatr fallback' * 3,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.thumb_up_alt_outlined),
+                  onPressed: () {},
+                ),
+                Text('12'),
+                IconButton(
+                  icon: Icon(Icons.mode_comment_outlined),
+                  onPressed: () {},
+                ),
+                Text('5'),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.share_outlined),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/Work_Tab/work_tab_screen.dart
+++ lib/Screen/Work_Tab/work_tab_screen.dart
@@ -1,10 +1,131 @@
 import 'package:flutter/material.dart';
+import 'Widget/open_source_card.dart';
 
 class WorkTabScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('work'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //TitleText('Past Experience'),
+              Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    width: double.infinity,
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TitleText('Open Source'),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            icon: Icon(Icons.chevron_right_outlined),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(maxHeight: 550),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return index == 0
+                                ? SizedBox(
+                                    width: 8,
+                                  )
+                                : Row(
+                                    children: [
+                                      OpenSourceCard(),
+                                      SizedBox(width: 10),
+                                    ],
+                                  );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.transparent,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    width: double.infinity,
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TitleText('Personal Projects'),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            icon: Icon(Icons.chevron_right_outlined),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(maxHeight: 550),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return index == 0
+                                ? SizedBox(
+                                    width: 8,
+                                  )
+                                : Row(
+                                    children: [
+                                      OpenSourceCard(),
+                                      SizedBox(width: 10),
+                                    ],
+                                  );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleText extends StatelessWidget {
+  final String title;
+
+  const TitleText(this.title);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 22),
     );
   }
 }
